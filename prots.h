456291ca6bb557a2f980c8551615cb7da18bffcf
kofany <j@dabrowski.biz>
2024-12-29 20:26:50 +0100

    sasl support added thanks to patrick@IRCnet @29-12-2024 20:26:50

--- prots.h
+++ prots.h
@@ -251,6 +251,7 @@ int rmdirext(const char *dir);
 void dumpIrcBacktrace();
 int decode_base64(unsigned char *dest, const char *src);
 char *encode_base64(int size, unsigned char *src);
+
 /* adns */
 void *__adns_work(void *);
 
